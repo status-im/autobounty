3ff0ae31cc98e05e1e4838d63ca1969051f82a43
Jordi Montes <jordimontes@acm.org>
2018-01-22 23:43:32 +0100

    Default and developing configurations ready

--- config/default.js
+++ config/default.js
@@ -1,23 +1,33 @@
 
 
-const BOUNTY_LABELS = {
-    'bounty-xs': 1,
-    'bounty-s': 10,
-    'bounty-m': 100,
-    'bounty-l': 1000,
-    'bounty-xl': 10000
-}
-
-
-
 module.exports = {
+    // Debug mode for testing the bot
     debug: true,
-    urlEndpoint: '/autobounty/fund',
-    logPath: './log/',
-    signerPath: 'https://ropsten.infura.io',
-    sourceAddress: 'XXXXX',
-    token: 'SNT',
-    gasLimit: 92000,
-    priceHour: 35,
-    bountyLabels: BOUNTY_LABELS
+    
+    // URL where the bot is listening (e.g. '/funding')
+    urlEndpoint: '',
+    
+    // Path for the log files (e.g. './log/')
+    logPath: '',
+    
+    // URL for the signer (e.g. 'https://ropsten.infura.io')
+    signerPath: '',
+    
+    // Address with the funding for the bounties 
+    sourceAddress: '',
+    
+    // Token of the currency for fetching real time prices (e.g. 'SNT')
+    token: '',
+    
+    // Limit for the gas used in a transaction (e.g. 92000)
+    gasLimit: 0,
+    
+    // Price per hour you will pay in dolars (e.g. 35)
+    priceHour: 0,
+    
+    // Delay before funding a bounty (e.g. 3600000)
+    delayInMiliSeconds: 0,
+    
+    // Bounty Labels for the issues and the correspondent houres (e.g. {'bounty-xs': 3})
+    bountyLabels: {}
 }
