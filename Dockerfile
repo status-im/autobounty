7a69695a2c26a66cf7775d61f009a19a47a75e6b
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-01-25 14:05:22 +0100

    Minor config changes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM node:7-onbuild
 ENV PORT 8080
 EXPOSE 8080
 
-ENV NAME autobounty
-ENV STANDARD_BOUNTY 0.001
-ENV WEBHOOK_SECRET test
+# Set this variable to the name of your production config file (without the extension)
+ENV NODE_ENV development
--- config/default.js
+++ config/default.js
@@ -3,31 +3,31 @@
 module.exports = {
     // Debug mode for testing the bot
     debug: true,
-    
+
     // URL where the bot is listening (e.g. '/funding')
     urlEndpoint: '',
-    
-    // Path for the log files (e.g. './log/')
+
+    // Path for the log files inside the docker image (e.g. './log/'), remember to create the folder inside the docker workspace if you change it (the folde will be copied to the docker image during the build)
     logPath: '',
-    
+
     // URL for the signer (e.g. 'https://ropsten.infura.io')
     signerPath: '',
-    
-    // Address with the funding for the bounties 
+
+    // Address with the funding for the bounties
     sourceAddress: '',
-    
+
     // Token of the currency for fetching real time prices (e.g. 'SNT')
     token: '',
-    
+
     // Limit for the gas used in a transaction (e.g. 92000)
     gasLimit: 0,
-    
+
     // Price per hour you will pay in dolars (e.g. 35)
     priceHour: 0,
-    
+
     // Delay before funding a bounty (e.g. 3600000)
     delayInMiliSeconds: 0,
-    
+
     // Bounty Labels for the issues and the correspondent houres (e.g. {'bounty-xs': 3})
     bountyLabels: {},
 
