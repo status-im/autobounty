73730df429e61cc7f05bbaaa83d259d44e3c4cef
Pol.Alvarez@BSC <pol.avms@gmail.com>
2018-02-27 16:53:53 +0100

    Cleaned dev.js config file

--- config/development.js
+++ config/development.js
@@ -11,12 +11,12 @@ module.exports = {
     urlEndpoint: '/',
     signerPath: 'https://ropsten.infura.io',
     sourceAddress: '0x26a4D114B98C4b0B0118426F10fCc1112AA2864d',
+    privateKey: '',
     token: 'SNT',
     gasLimit: 92000,
     priceHour: 1,
     delayInMiliSeconds: 10000,
     bountyLabels: BOUNTY_LABELS,
-    githubUsername: 'jomsdev',
-    privateKey: 'f1255c25e571c84b894ac8b7e3d74763215320eb6b4a7a70dd67f59ebddb56a8'
+    githubUsername: '',
 
 }
